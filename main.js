ae351eff587870385ebcbdc513b2808f7f2685ee
Enem <craigcharltonlunga1000@gmail.com>
2022-11-30 15:03:12 -0800

    init commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction(){
+const mobile__nav = document.getElementById("mobile__nav");
+if(mobile__nav.style.display === "none"){
+    mobile__nav.style.display = "block";
+}else{
+    mobile__nav.style.display = "none";
+}
+}
